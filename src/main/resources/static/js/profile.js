628494cc75d77bb5e38c073ff9d925afe3ad2872
Marius Voina <aniov_usa@yahoo.com>
2017-07-24 12:21:42 +0300

    ...

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -5,4 +5,7 @@ window.onload = function(){
     $.get("../static/footer.html", function(data){
         $("#footer-placeholder").replaceWith(data);
     });
+    $.get("../static/googlemap.html", function(data){
+        $("#google-placeholder").replaceWith(data);
+    });
 }
