e702970a1120dbf2aab0b89e262418c0d50b6ee0
Marius Voina <aniov_usa@yahoo.com>
2017-08-06 13:09:37 +0300

    ...

--- src/main/resources/static/js/usermainpage.js
+++ src/main/resources/static/js/usermainpage.js
@@ -9,18 +9,7 @@ window.onload = function () {
         $("#footer-placeholder").replaceWith(data);
     });
 
-    $.ajax({
-        url: "/user",
-        type: "GET",
-        contentType: "application/json; charset=utf-8",
-        success: function (data, textStatus, jqXHR) {
-            console.log("User ata: " + data.username)
-            $("#userName").html(data.username);
-        },
-        error: function (data, textStatus, jqXHR) {
-            console.log("Cannot read username");
-        }
-    });
+    getAuthUserName();
 
     $.ajax({
         url: "/profiles",
@@ -78,4 +67,21 @@ function createCards() {
             }
         });
     }
+}
+
+function getAuthUserName() {
+
+    $.ajax({
+        url: "/user",
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        success: function (data, textStatus, jqXHR) {
+            console.log("User ata: " + data.username)
+            $("#userName").html(data.username);
+        },
+        error: function (data, textStatus, jqXHR) {
+            console.log("Cannot read username");
+        }
+    });
+    
 }
