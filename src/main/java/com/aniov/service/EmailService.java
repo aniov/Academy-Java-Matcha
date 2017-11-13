802b6c24d11e6bd4d82542e79f0493c3de8d9813
aniov <aniov@yahoo.com>
2017-11-14 01:47:09 +0200

    application-dev

--- src/main/java/com/aniov/service/EmailService.java
+++ src/main/java/com/aniov/service/EmailService.java
@@ -70,9 +70,8 @@ public class EmailService {
 
         if (send) {
             sendEmail(user.getEmail(), subject, sw.toString());
+            System.out.println("EMAIL: " + user.getEmail());
         }
-
-        System.out.println("EMAIL: " + user.getEmail());
     }
 
     public void resetPasswordToken(User user) throws MessagingException {
